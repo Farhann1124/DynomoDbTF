7b7366c9d52687d0a51a51ffa3692cdc1de310e3
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-07 21:07:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+resource "aws_dynamodb_table" "my_table" {
+  name           = var.table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
