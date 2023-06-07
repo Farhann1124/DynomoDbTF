9f6fce5915308a8646b7c5b901cf4fe8e14817bc
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-07 22:00:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,12 @@
-provider "aws" {
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
-  region     = var.aws_region
+terraform {
+  required_version = ">= 1.0.0"
 }
-
-#resource "aws_dynamodb_table" "my_table" {
- # terraform {
- # required_version = ">= 1.0.0"
-#}
 
 provider "aws" {
   region = var.region
 }
 
-resource "aws_dynamodb_table" "my_table" {
+resource "aws_dynamodb_table" "example_table" {
   name           = var.table_name
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
@@ -23,24 +16,17 @@ resource "aws_dynamodb_table" "my_table" {
   }
 }
 
-# Skip the aws_dynamodb_table_item resource here
+resource "aws_dynamodb_table_item" "example_items" {
+  table_name = aws_dynamodb_table.example_table.name
 
-# Run the AWS CLI commands to populate data
-# This will be executed during the "terraform apply" step in GitHub Actions
-resource "null_resource" "populate_data" {
-  provisioner "local-exec" {
-    command = <<EOT
-aws dynamodb put-item \
-  --region ${var.region} \
-  --table-name ${aws_dynamodb_table.example_table.name} \
-  --item '{"id": {"N": "1"}, "name": {"S": "John"}, "age": {"N": "30"}}'
-
-aws dynamodb put-item \
-  --region ${var.region} \
-  --table-name ${aws_dynamodb_table.example_table.name} \
-  --item '{"id": {"N": "2"}, "name": {"S": "Jane"}, "age": {"N": "25"}}'
-EOT
+  item {
+    id   = "1"
+    name = "John"
+    age  = 30
+  }
+  item {
+    id   = "2"
+    name = "Jane"
+    age  = 25
   }
 }
-
-
